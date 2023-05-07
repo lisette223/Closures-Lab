3f2ae2530746c1f43e2593b62190f6c60865962d
LisetteAntigua <antigua223@gmail.com>
2023-05-07 14:22:38 -0400

    Initial Commit

---
+++ Closures Lab.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+/*:
+1) Create a closure assigned to a constant fourClosure that has no parameters and returns an Int. The body of the closure should always return the value 4. Call the closure once times..*/
+
+/*:
+2) Write a closure assigned to a constant square that takes an integer as input and returns its square */
+
+/*:
+3) Write a closure assigned to a constant sum that takes two integers as input and returns their sum:
+*/
+
+/*:
+4) Write  a closure assigned to a constant sumOfEvens that takes an array of integers as input and returns the sum of all even numbers
+*/
+
+
+
+/*:
+5) Using the code below, use the map function to create an array of Int values, whose values are equal to the original integer value, plus 1. Use $0 as you iterate through the values of the array. Print the resulting collection.
+*/
+let testScores = [65,80, 88, 90, 47]
+
+
+
+/*:
+6) Using the code below, use the filter function to create a new array of String values. The new array should only include Strings longer than four characters. Use $0 as you iterate through the values of the array.  Print the resulting collection.
+*/
+
+let subjects = ["Math", "Computer Science", "English", "Chemistry", "History", "Gym"]
+
+
+/*:
+7) Using the code below, use the reduce function to subtract all of the values within the array from the starting value 100. Print the resulting value.
+*/
+
+let damageTaken = [25, 10, 15, 20, 0]
+
+
